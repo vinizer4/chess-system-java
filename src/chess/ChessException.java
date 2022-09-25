d037c9c97c0016e583a3e778e6164801bf756bc2
vinizera <vinicius.ts.online@gmail.com>
2022-09-25 20:06:10 -0300

    Moving pieces

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,5 +1,11 @@
 package chess;
 
-public class ChessException {
+import boardgame.BoardException;
 
+public class ChessException extends BoardException {
+	private static final long serialVersionUID = 1L;
+	
+	public ChessException(String msg) {
+		super(msg);
+	}
 }
